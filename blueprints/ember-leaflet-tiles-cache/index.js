b802014973148b9d3edd835208ff97628ddef7bd
posadchiy <posadchiy@gmail.com>
2016-07-20 13:57:37 +0300

    supress error 'command requires an entity name to be specified'

--- blueprints/ember-leaflet-tiles-cache/index.js
+++ blueprints/ember-leaflet-tiles-cache/index.js
@@ -2,6 +2,12 @@
 module.exports = {
   description: 'install Leaflet.TileLayer.PouchDBCached as bower dep',
 
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
+
   afterInstall: function(options) {
     return this.addBowerPackageToProject(
       'Leaflet.TileLayer.PouchDBCached',
